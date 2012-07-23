671f5d4e1b7fe8ca3398f22d37d9c261f73e26f1
Jesly Varghese <furiousjezz@gmail.com>
2012-07-24 01:04:00 +0530

    tweaking friendlist

--- facebook.rb
+++ facebook.rb
@@ -10,10 +10,11 @@ module Facebook
 
 	def self.upload_photo(params)
 		tagged =[]
+		puts params
 		params[:user_list].each do |user|
 			fb_user = FbGraph::User.fetch(user)
 			tagged<< FbGraph::Tag.new(
-    		:id => fb_user.identifier.to_i,
+    		#:id => fb_user.identifier.to_i,
     		:name =>fb_user.name,
     		:x => 20+Random.rand(90),
     		:y => 10+Random.rand(90)
@@ -22,34 +23,11 @@ module Facebook
 	    
 	    photo = FbGraph::User.me(params[:access_token]).photo!(
 			:source=> File.new(File.join(File.dirname(__FILE__), params[:file_name])),
-			:message=>params[:message],
-			:tags=>tagged
+			:tags=>tagged,
+			:message=>params[:message]
 			)
 	    	puts "Pic uploaded......"
 	    	puts photo
-	    	#Facebook::log_post fb_user.identifier.to_i, params[:file_name]
-	    	#ActiveRecord::Base.connection.close
-		#case secrecy
-		#	when "coverpic" 
-		#		Facebook::as_cover albums
-		#	when "public"
-		#		Facebook::as_wall_pic albums
-		#	when "secret"
-		#		Facebook::as_private_message
-		#end
-	end
-
-	def self.as_cover album
-	  album = albums.detect{ |album| album.type == "cover_photo"}
-	  album
-	end
-
-	def self.as_wall_pic
-	  album = albums.detect{ |album| album.type == "cover_photo"}
-	  album
-	end
-
-	def self.as_private_message
 	end
 
 	def self.user(uid)
