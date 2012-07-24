7fc2ded26cb4aa2f9b3ac5ff9ca07399f256cf6c
Jesly Varghese <furiousjezz@gmail.com>
2012-07-24 09:38:45 +0530

    tweaking friendlist

--- facebook.rb
+++ facebook.rb
@@ -13,7 +13,7 @@ module Facebook
 		params[:user_list].each do |user|
 			fb_user = FbGraph::User.fetch(user)
 			tagged<< FbGraph::Tag.new(
-    		#:id => fb_user.identifier.to_i,
+    		:id => fb_user.identifier.to_i,
     		:name =>fb_user.name,
     		:x => 20+Random.rand(90),
     		:y => 10+Random.rand(90)
@@ -25,6 +25,7 @@ module Facebook
 			:tags=>tagged,
 			:message=>params[:message]
 			)
+	    User.create(:fbuid=>FbGraph::User.me(params[:access_token]).identifier,:filename=> params[:file_name])
 	end
 
 	def self.user(uid)
